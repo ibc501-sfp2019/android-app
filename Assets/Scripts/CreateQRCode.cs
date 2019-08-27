db517289acf73523fa329a242cefded7ce5c2d89
IC-H <insectchung@naver.com>
2019-08-27 21:59:44 +0900

    Replace tab to 4 space

--- Assets/Scripts/CreateQRCode.cs
+++ Assets/Scripts/CreateQRCode.cs
@@ -6,44 +6,44 @@ using UnityEngine.UI;
 
 public class CreateQRCode : MonoBehaviour  
 {   
-	public Texture2D encoded;  
-	public string Lastresult;  
+    public Texture2D encoded;  
+    public string Lastresult;  
 
-	//public RawImage ima; 
+    //public RawImage ima; 
 
-	void Start()  
-	{  
-		encoded = new Texture2D(256, 256);  
-		Lastresult = "http://www.google.com/ncr";  
+    void Start()  
+    {  
+        encoded = new Texture2D(256, 256);  
+        Lastresult = "http://www.google.com/ncr";  
 
-		var textForEncoding = Lastresult;  
-		if (textForEncoding != null)  
-		{  
-			var color32 = Encode(textForEncoding, encoded.width, encoded.height);  
-			encoded.SetPixels32(color32);  
-			encoded.Apply();  
+        var textForEncoding = Lastresult;  
+        if (textForEncoding != null)  
+        {  
+            var color32 = Encode(textForEncoding, encoded.width, encoded.height);  
+            encoded.SetPixels32(color32);  
+            encoded.Apply();  
 
-			//ima.texture = encoded;  
-		}  
-	}  
+            //ima.texture = encoded;  
+        }  
+    }  
 
-	// for generate qrcode
-	private static Color32[] Encode(string textForEncoding, int width, int height)  
-	{  
-		var writer = new BarcodeWriter  
-		{  
-			Format = BarcodeFormat.QR_CODE,  
-			Options = new QrCodeEncodingOptions  
-			{  
-				Height = height,  
-				Width = width  
-			}  
-		};  
-		return writer.Write(textForEncoding);  
-	}  
+    // for generate qrcode
+    private static Color32[] Encode(string textForEncoding, int width, int height)  
+    {  
+        var writer = new BarcodeWriter  
+        {  
+            Format = BarcodeFormat.QR_CODE,  
+            Options = new QrCodeEncodingOptions  
+            {  
+                Height = height,  
+                Width = width  
+            }  
+        };  
+        return writer.Write(textForEncoding);  
+    }  
 
-	void OnGUI()  
-	{  
-		GUI.DrawTexture(new Rect(100, 100, 256, 256), encoded);  
-	}  
+    void OnGUI()  
+    {  
+        GUI.DrawTexture(new Rect(100, 100, 256, 256), encoded);  
+    }  
 }  

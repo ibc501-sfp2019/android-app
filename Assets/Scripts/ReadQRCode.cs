9dfaab3771476f62f956b39f6b39e25b85f8ebda
tomriddle7 <tomriddle7@naver.com>
2019-09-15 20:07:54 +0900

    set camera position and rotation

--- Assets/Scripts/ReadQRCode.cs
+++ Assets/Scripts/ReadQRCode.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using LitJson;
 
 public class ReadQRCode : MonoBehaviour
 {
     public Text UIText;
+    public Camera camera;
 
     // Start is called before the first frame update
     void Start()
     {
+        JsonData jsonData = JsonMapper.ToObject(GameManager.instance.readJson);
+        //set camera position
+        float x = float.Parse(jsonData["location"]["x"].ToString());
+        float y = float.Parse(jsonData["location"]["y"].ToString());
+        float z = float.Parse(jsonData["location"]["z"].ToString());
+        camera.transform.position = new Vector3(x, y, z);
+        //set camera rotation
+        x = float.Parse(jsonData["rotation"]["x"].ToString());
+        y = float.Parse(jsonData["rotation"]["y"].ToString());
+        z = float.Parse(jsonData["rotation"]["z"].ToString());
+        float w = float.Parse(jsonData["rotation"]["w"].ToString());
+        camera.transform.rotation = new Quaternion(x, y, z, w);
+
         UIText.text = GameManager.instance.readJson;
     }
 }

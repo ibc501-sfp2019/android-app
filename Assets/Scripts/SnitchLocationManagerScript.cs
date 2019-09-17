8351a6f26c1a66d31bf4f91ee82bb8adb5cd1086
IC-H <insectchung@naver.com>
2019-09-17 21:25:43 +0900

    【App】SnitchWorldレベル作成

---
+++ Assets/Scripts/SnitchLocationManagerScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using LitJson;
+using CommonScript;
+using Enum;
+
+public struct LocationInfo
+{
+    public double time;
+    public Vector3 position;
+    public LocationInfo(double time, Vector3 position) {
+        this.time = time;
+        this.position = position;
+    }
+}
+
+public class SnitchLocationManagerScript : MonoBehaviour
+{
+    [SerializeField]
+    private int playId=1;
+    [SerializeField]
+    public string serverDomain="http://133.11.59.240:8000";
+
+    private Dictionary<int, SnitchMovementScript> snitchMovementScriptDict;
+
+    private PeriodicRoutine periodicRoutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        snitchMovementScriptDict = new Dictionary<int, SnitchMovementScript>();
+        foreach(GameObject snitch in GameObject.FindGameObjectsWithTag("Snitch")) {
+            SnitchMovementScript script = snitch.GetComponent<SnitchMovementScript>();
+            snitchMovementScriptDict.Add(script.snitchId, script);
+        }
+        periodicRoutine = new PeriodicRoutine(0.5f);
+        periodicRoutine.setRoutine(updateLocationInfos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(periodicRoutine.run());
+    }
+
+    IEnumerator updateLocationInfos()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(serverDomain + "/observe/?play_id=" + playId);
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError) {
+            Debug.Log("Network Error Occured with error : " + request.error);
+            yield break;
+        }
+        JsonData response = JsonMapper.ToObject(request.downloadHandler.text);
+        if (!response.IsObject) {
+            Debug.Log("Json Parse Error");
+            yield break;
+        }
+        if ((int) ResponseStatus.OK != (int) response["status"]) {
+            Debug.Log("Status is not 200, status : " + response["status"]);
+            yield break;
+        }
+        foreach(JsonData ballInfo in response["data"]) {
+            SnitchMovementScript script = snitchMovementScriptDict[(int) ballInfo["ball_id"]];
+            List<LocationInfo> preInfos = script.locationInfos;
+            List<LocationInfo> list = new List<LocationInfo>();
+            double lastestTime = 0;
+            if (preInfos != null) {
+                list = preInfos;
+                lastestTime = list[list.Count - 1].time;
+            }
+            if (list.Count > 200) continue;
+            foreach (JsonData info in ballInfo["data"]) {
+                double timestamp = double.Parse(info["timestamp"].ToString());
+                if (lastestTime > timestamp) continue;
+
+                float x =  float.Parse(info["location"]["x"].ToString());
+                float y =  float.Parse(info["location"]["y"].ToString());
+                float z =  float.Parse(info["location"]["z"].ToString());
+                Vector3 position = new Vector3(x, y, z);
+                LocationInfo loc = new LocationInfo(timestamp, position);
+                list.Add(loc);
+            }
+            script.locationInfos = list;
+        }
+        Debug.Log("Update Locations");
+    }
+}
---
+++ Assets/Scripts/SnitchMovementScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using CommonScript;
+
+public class SnitchMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    public int snitchId;
+
+    [SerializeField]
+    public int size=0;
+    public List<LocationInfo> locationInfos;
+
+    private LocationInfo currentLocationInfo;
+
+    private float eps = 0.05f;
+
+    private PeriodicRoutine periodicRoutine;
+
+    IEnumerator arrangeLocationInfos()
+    {
+        if (locationInfos == null || locationInfos.Count < 1) yield break;
+        // Debug.Log("start");
+        Double now = (DateTime.UtcNow - new DateTime(1970,1,1,0,0,0, DateTimeKind.Utc)).TotalSeconds;
+        foreach (LocationInfo location in locationInfos)
+        {
+            if (location.time < now) {
+                locationInfos.Remove(location);
+                yield return null;
+            }
+            if ((location.time - now) < eps) {
+                currentLocationInfo = location;
+            }
+            size = locationInfos.Count;
+            yield break;
+        }
+        // Debug.Log("end");
+    }
+
+    private void move()
+    {
+        if ((object) currentLocationInfo == null) return;
+        transform.position = currentLocationInfo.position;
+    }
+
+    void Start()
+    {
+        periodicRoutine = new PeriodicRoutine(0.05f);
+        periodicRoutine.setRoutine(arrangeLocationInfos);
+    }
+
+    void FixedUpdate()
+    {
+        move();
+    }
+
+    void Update() {
+        StartCoroutine(periodicRoutine.run());
+    }
+}

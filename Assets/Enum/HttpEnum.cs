d5d6bbe90ccc9df486d5d66e469d04d24a290eb0
IC-H <insectchung@naver.com>
2019-09-17 21:21:15 +0900

    【App】HTTPリスポンスEnum作成

---
+++ Assets/Enum/HttpEnum.cs
@@ -0,0 +1,11 @@
+
+namespace Enum
+{
+    public enum ResponseStatus
+    {
+        OK                      = 200,
+        FORBIDDEN               = 403,
+        METHOD_NOT_ALLOWED      = 405,
+        INTERNER_SERVER_ERROR   = 500
+    }
+}
